9e5e1f4aa4110cb3e080d2bc18433bf09f7469e0
MostOfLuck <burtman.samuel26@gmail.com>
2024-03-30 18:41:50 +0100

    update main.js

--- main.js
+++ main.js
@@ -48,3 +48,45 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+function applyTranslations(language) {
+    var elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(function(element) {
+        var key = element.getAttribute('data-i18n');
+        if (translations[key] && translations[key][language]) {
+            element.textContent = translations[key][language];
+        }
+    });
+}
+
+// Применяем переводы при загрузке страницы
+document.addEventListener('DOMContentLoaded', function() {
+    applyTranslations('en'); // По умолчанию применяем переводы на английский язык
+});
+
+// Применяем переводы при переключении языка
+document.getElementById('language-toggle').addEventListener('change', function() {
+    var language = this.checked ? 'ru' : 'en'; // Проверяем состояние переключателя
+    applyTranslations(language); // Применяем соответствующие переводы
+});
+
+var translations = {
+    "header.title": {
+        "en": "We are dedicated to your success and are always ready to help.",
+        "ru": "Мы посвящаемся вашему успеху и всегда готовы помочь."
+    },
+    "categories.title": {
+        "en": "What we're doing",
+        "ru": "Что мы делаем"
+    },
+    "paragraph.p": {
+        "en": "We don't just make websites, we bring customers into the business",
+        "ru": "Мы не просто делаем сайты, мы привлекаем клиентов в бизнес."
+    },
+    "button_get_started": {
+        "en": "DISCUSS THE PROJECT",
+        "ru": "ОБСУДИТЬ ПРОЕКТ"
+    },
+
+    // Добавьте другие переводы здесь
+};
