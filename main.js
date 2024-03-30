043c53d655229fcae701c4caffd00df17456b6a6
MostOfLuck <burtman.samuel26@gmail.com>
2024-03-30 19:29:51 +0100

    update main.js

--- main.js
+++ main.js
@@ -71,6 +71,18 @@ document.getElementById('language-toggle').addEventListener('change', function()
 });
 
 var translations = {
+    "home": {
+        "en": "Home",
+        "ru": "Главная"
+    },
+    "about": {
+        "en": "About",
+        "ru": "О Нас"
+    },
+    "contact": {
+        "en": "Contact",
+        "ru": "Связь"
+    },
     "header.title": {
         "en": "We are dedicated to your success and are always ready to help.",
         "ru": "Мы посвящаемся вашему успеху и всегда готовы помочь."
@@ -86,6 +98,27 @@ var translations = {
     "button_get_started": {
         "en": "DISCUSS THE PROJECT",
         "ru": "ОБСУДИТЬ ПРОЕКТ"
+    },
+    "achievements": {
+        "en": "Achievements",
+        "ru": "Достижения"
+    },
+    "paragraph_desc": {
+        
+        "en": "We have been on the market for over 5 years and have over 20 different products and 300 satisfied customers. Also, we have awards to prove our expertise.",
+        "ru": "Мы работаем на рынке уже более 5 лет, у нас более 20 различных продуктов и 300 довольных клиентов. Кроме того, у нас есть награды, подтверждающие нашу компетентность."
+    },
+    "services": {
+        "en": "Services",
+        "ru": "Услуг"
+    },
+    "customers": {
+        "en": "Customers",
+        "ru": "Клиентов"
+    },
+    "awards": {
+        "en": "Awards",
+        "ru": "Наград"
     },
 
     // Добавьте другие переводы здесь
