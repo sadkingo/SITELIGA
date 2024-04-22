aec42573b47fa639a6e8372a6f261e771e85ea26
MostOfLuck <burtman.samuel26@gmail.com>
2024-04-22 11:54:43 +0200

    update bg.js

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ particlesJS('particles-js', {
 			type: 'circle',
 			stroke: { width: 0, color: '#000000' },
 			polygon: { nb_sides: 5 },
-			image: { src: 'img/github.svg', width: 100, height: 100 },
+			image: { src: 'img/github.svg', width: 100, height: 1200 },
 		},
 		opacity: {
 			value: 0.5,
